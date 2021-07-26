c8eb27adc1f9bacd186a1268c5fe51dff52bcc79
simon creek <simoncreek@tutanota.com>
2021-07-27 01:29:17 +0900

    Added player data to types file

--- Assets/Scripts/Types.cs
+++ Assets/Scripts/Types.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 [Serializable]
 public enum DNodeType
@@ -43,4 +44,28 @@ public enum Variant
 {
 	PLUS,
 	MINUS
+}
+
+// This class is for data save and loading
+[System.Serializable]
+public class PlayerData
+{
+    public PlayerStatusData StatusData;
+    public Dictionary<string, LocationData> LocationMap;
+}
+
+[System.Serializable]
+public class LocationData
+{
+	public string LocationName {get; set;}
+	// TODO 
+	// HMM this looks complicated
+}
+
+[System.Serializable]
+public class PlayerStatusData
+{
+    public List<string> Logs = new List<string>();
+    public PlayerStats Stats = new PlayerStats();
+    public List<string> Equipped = new List<string>();
 }
